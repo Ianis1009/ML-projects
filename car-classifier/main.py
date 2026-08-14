67cec8691fcd13aca46b13d742489451fbfabc51
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-14 22:45:58 +0300

    train and test

--- car-classifier/main.py
+++ car-classifier/main.py
@@ -7,10 +7,22 @@ from sklearn.tree import DecisionTreeClassifier
 
 BASE_DIR = Path(__file__).resolve().parent
 DATA_PATH = BASE_DIR / "data" / "cars.csv"
-data = pd.read_csv(DATA_PATH)
+data = pd.read_csv(DATA_PATH) # read csv
 
 print("DATASET:")
 print(data.head())
 
 print()
 print(f"Number of cars: {len(data)}")
+
+# features
+features = ["engine", "doors", "seats", "horsepower"]
+
+X = data[features]
+y = data["type"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y) 
+
+print()
+print(f"Training samples: {len(X_train)}")
+print(f"Test samples: {len(X_test)}")
+
