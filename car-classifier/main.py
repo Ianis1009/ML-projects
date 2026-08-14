8e911784f8f5987ac1fb8e9b2a25dc0196d4eb3a
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-14 22:51:07 +0300

    predict type

--- car-classifier/main.py
+++ car-classifier/main.py
@@ -36,3 +36,20 @@ print(f"Accuracy: {accuracy:.2%}")
 print()
 print("Report:")
 print(classification_report(y_test, predictions))
+
+
+# Car prediction
+
+print()
+print("Car Classifier")
+
+engine = float(input("Engine size (e.g. 2.0)"))
+doors = int(input("Number of doors: "))
+seats = int(input("Number of seats: "))
+horsepower = int(input("Horsepower: "))
+
+new_car = pd.DataFrame([{"engine": engine,"doors": doors, "seats": seats, "horsepower": horsepower}])
+
+prediction = model.predict(new_car)[0]
+print()
+print(f"Predicted type: {prediction}") # by cars.csv
