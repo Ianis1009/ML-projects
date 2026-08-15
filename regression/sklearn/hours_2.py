4052f6c419beca41d924efe8ffc66c917c41178e
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-15 19:13:56 +0300

    predictions

--- regression/sklearn/hours_2.py
+++ regression/sklearn/hours_2.py
@@ -23,3 +23,15 @@ print(f"Coefficient for sleep: {model.coef_[1]:.2f}")
 print(f"Intercept: {model.intercept_:.2f}")
 
 
+new_student = [[6, 8]]
+prediction = model.predict(new_student)
+print()
+print(f"Predicted grade: {prediction[0]:.2f}")
+
+predictions = model.predict(X)
+print()
+print("All preditions: ")
+
+for actual, predicted in zip(y, predictions):
+    print(f"Actual: {actual:5.1f} " f"| Predicted: {predicted: 5.1f}")
+
