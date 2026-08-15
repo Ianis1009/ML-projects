62e3bf363b882a08dcfb1a9be6156998bd38efc1
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-16 00:50:48 +0300

    update tree.h

--- decision-tree/tree.h
+++ decision-tree/tree.h
@@ -1,1 +1,15 @@
-//TODO
+
+#ifndef TREE_H
+#define TREE_H
+
+typedef struct {
+    int feature;
+    double gini;
+    double threshold;
+} Split;
+
+double gini (const int *y, int n, int num_classes);
+
+Split find_best_split (const double X[][2], const int *y, int n_samples, int n_features, int num_classes);
+
+#endif
