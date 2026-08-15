9ca9f71126b5bc7b991be2dc3692f3e4cf855eb9
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-15 18:28:49 +0300

    add hours.py

--- regression/sklearn/hours.py
+++ regression/sklearn/hours.py
@@ -1,1 +1,18 @@
-#TODO
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# dataset
+hours = [1, 2, 3, 4, 5, 6, 7, 8]
+grades = [52, 55, 61, 65, 70, 74, 81, 86]
+
+# sklearn -> X should be 2D
+
+X = [[1], [2], [3], [4], [5], [6], [7], [8]]
+
+y = grades
+
+# model 
+model = LinearRegression()
+
+# train model
+model.fit(X, y)
