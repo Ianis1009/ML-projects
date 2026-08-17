cb4a0cb76aca9962a9072a7ac534ca1a8af3b37a
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-17 17:41:45 +0300

    init gini

--- decision-tree/tree.c
+++ decision-tree/tree.c
@@ -2,4 +2,23 @@
 #include <float.h>
 #include "tree.h"
 
-//TODO
+/***
+ * 
+ * Gini = 1 - sum (p_i ^ 2)
+ * 
+ */
+
+double gini (const int *y, int n, int num_classes) {
+
+    int counts[num_classes];
+    // init
+    for (int i = 0; i <= num_classes; i++ ) {
+        counts[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++ ) {
+        counts[y[i]]++;
+    }
+
+    
+}
